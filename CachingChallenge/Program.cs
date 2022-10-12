d4139c06a4dbfb24d278f8c3492d509e8bcc17b6
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-12 20:13:23 +0200

    Add project files.

---
+++ CachingChallenge/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace CachingChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DataAccess db = new DataAccess();
+
+            DataAccessCache db = new DataAccessCache();
+
+            //var people = db.SimulatedPersonListLookup();
+
+            //people = db.SimulatedPersonListLookup();
+            //people = db.SimulatedPersonListLookup();
+
+            //db.SimulatedPersonById(3);
+            //db.SimulatedPersonById(5);
+            //db.SimulatedPersonById(3);
+            //db.SimulatedPersonById(3);
+            //db.SimulatedPersonById(3);
+            //db.SimulatedPersonById(3);
+
+            db.SimulatedPersonListByLastName("Jones");
+            db.SimulatedPersonListByLastName("Storm");
+            db.SimulatedPersonListByLastName("Jones");
+            db.SimulatedPersonListByLastName("Storm");
+            db.SimulatedPersonListByLastName("Jones");
+            db.SimulatedPersonListByLastName("Storm");
+
+            Console.ReadLine();
+        }
+    }
+}
